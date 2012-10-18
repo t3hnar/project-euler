b187afa38748530fe96c89ebd4b1085f8c65d495
Yaroslav Klymko <t3hnar@gmail.com>
2012-10-18 17:59:05 +0300

    Resolve problem 12 using numberOfDivisor function

---
+++ src/main/scala/ua/t3hnar/project/euler/p012/Problem12.scala
@@ -0,0 +1,40 @@
+package ua.t3hnar.project.euler
+package p012
+
+/**
+ * @author Yaroslav Klymko
+ */
+object Problem12 extends App {
+
+  object NaturalNumbers {
+    def apply(): Stream[Long] = apply(1)
+
+    def apply(from: Long): Stream[Long] = from #:: apply(from + 1)
+  }
+
+  object TriangleNumbers {
+    def apply(): Stream[Long] = {
+      val naturals = NaturalNumbers(2).iterator
+      def loop(x: Long): Stream[Long] = x #:: loop(x + naturals.next())
+      loop(1)
+    }
+  }
+
+
+  def numberOfDivisors(n: Long): Int = {
+    val sqrt = math.sqrt(n)
+
+    def loop(i: Int, nod: Int): Int =
+      if (i > sqrt) nod
+      else if (n % i == 0) loop(i + 1, nod + 2)
+      else loop(i + 1, nod)
+
+    val nod = loop(1, 0)
+    if (sqrt * sqrt == n) nod - 1 else nod
+  }
+
+
+  assert(TriangleNumbers()(6) == 28)
+  assert(TriangleNumbers().find(numberOfDivisors(_) > 5).get == 28)
+  assert(TriangleNumbers().find(numberOfDivisors(_) >  500).get == 76576500)
+}
