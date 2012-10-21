7e20e03d9056fe3ddc5ed62d8d4c2c88c8d6a6b6
Yaroslav Klymko <t3hnar@gmail.com>
2012-10-21 18:21:57 +0300

    Improve solution 13

--- src/main/scala/ua/t3hnar/project/euler/p013/Problem13.scala
+++ src/main/scala/ua/t3hnar/project/euler/p013/Problem13.scala
@@ -8,5 +8,5 @@ import io.Source
  */
 object Problem13 extends App {
   val lines = Source.fromInputStream(getClass.getResourceAsStream("numbers.txt")).getLines()
-  assert(lines.map(_.take(11).toLong).sum.toString.take(10) == "5537376230")
+  assert(lines.map(BigInt(_)).sum.toString.take(10) == "5537376230")
 }
